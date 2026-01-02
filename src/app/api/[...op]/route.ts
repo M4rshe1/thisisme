c9c9ade6e8a3bb82923a38fce69aff555f6f5d41
M4rshe1 <colin.heggli@gmail.com>
2026-01-02 10:44:10 +0100

    update pkgs

--- src/app/api/[...op]/route.ts
+++ src/app/api/[...op]/route.ts
@@ -1,5 +1,4 @@
-import { createNextRouteHandler } from "@openpanel/nextjs/server";
-
-export const POST = createNextRouteHandler({
+import { createRouteHandler } from "@openpanel/nextjs/server";
+export const { POST, GET } = createRouteHandler({
   apiUrl: process.env.NEXT_PUBLIC_OPENPANEL_API_URL as string,
 });
